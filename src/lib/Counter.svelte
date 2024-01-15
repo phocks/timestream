9c60da268d86551a53e285190fc01824ec9cb4e3
Joshua Byrd <phocks@gmail.com>
2024-01-15 19:27:27 +1000

    Seconds

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   let currentUnixTime = $state(Math.floor(Date.now() / 1000));
   let timeInFuture = $state(1705330800);
+  let interval: any;
 
   const secondsToDhms = (seconds: number) => {
     seconds = Number(seconds);
@@ -12,21 +13,23 @@
     var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
     var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
     var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "0 seconds";
     return dDisplay + hDisplay + mDisplay + sDisplay;
-  }
+  };
 
   $effect(() => {
-    setInterval(() => {
+    interval = setInterval(() => {
       currentUnixTime = Math.floor(Date.now() / 1000);
     }, 1000);
 
     return () => {
       console.log("cleanup!");
+      clearInterval(interval);
     };
   });
 
   let timeDiff = $derived(timeInFuture - currentUnixTime);
+  $inspect(timeDiff, "timeDiff");
 </script>
 
 <div>{secondsToDhms(timeDiff)}</div>
